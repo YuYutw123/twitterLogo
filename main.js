50561cbc8881ca774f2e11cc22e0ce6e419acc50
yuyutw123 <yuyutw123@gmail.com>
2023-07-25 09:58:31 +0800

    message

---
+++ main.js
@@ -0,0 +1,3 @@
+document.querySelector('link[rel*="icon"]').href = "https://pbs.twimg.com/media/F12BhZ7aEAIc1bQ?format=png&name=small";
+document.querySelector('link[rel*="shortcut icon"]').href = "https://pbs.twimg.com/media/F12BhZ7aEAIc1bQ?format=png&name=small";
+document.querySelector('link[rel*="apple-touch-icon"]').href = "https://pbs.twimg.com/media/F12BhZ7aEAIc1bQ?format=png&name=small";
