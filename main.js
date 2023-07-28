c3e948680eaeee2cdaf88f57d2c841f0eb7396b7
yuyutw123 <yuyutw123@gmail.com>
2023-07-28 14:12:52 +0800

    fix title X

--- main.js
+++ main.js
@@ -1,3 +1,31 @@
 document.querySelector('link[rel*="icon"]').href = "https://pbs.twimg.com/media/F12BhZ7aEAIc1bQ?format=png&name=small";
 document.querySelector('link[rel*="shortcut icon"]').href = "https://pbs.twimg.com/media/F12BhZ7aEAIc1bQ?format=png&name=small";
 document.querySelector('link[rel*="apple-touch-icon"]').href = "https://pbs.twimg.com/media/F12BhZ7aEAIc1bQ?format=png&name=small";
+
+function sleep(time) {
+    return (new Promise(function (resolve, reject) {
+        setTimeout(function () { resolve(); }, time);
+    }));
+}
+sleep(1000).then(function() {
+    loading();
+});
+document.body.onclick = function (e) {
+    loading();
+};
+async function loading() { 
+    sleep(1000).then(function () {
+        let titleDOM = document.querySelector('title').textContent;
+        let prefix = '';
+        for (let i = 0; i < titleDOM.length; i++) { 
+            if (titleDOM[i] != 'X') {
+                prefix += titleDOM[i];
+            } else {
+                prefix += "Twitter";
+                break;
+            }
+        }
+        console.log(prefix);
+        document.title = prefix;
+    })
+}
